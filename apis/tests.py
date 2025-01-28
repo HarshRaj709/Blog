581525361e45c554c698e144a334c12228680ee1
HarshRaj709 <harshahu709@gmail.com>
2025-01-28 10:24:19 +0530

    all test cases

--- apis/tests.py
+++ apis/tests.py
@@ -9,15 +9,17 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 
 class APIEndpointsTests(APITestCase):
     def setUp(self):
-        # Set up users, blogs, and categories for testing
         self.user = User.objects.create_user(username="testuser", password="password123")
-        #self.admin_user = User.objects.create_superuser(username="admin", password="admin123")
+        
         self.category = Category.objects.create(category_name="Test Category")
         self.blog = Blog.objects.create(title="Test Blog", content="Test content", category=self.category, writer=self.user)
         self.refresh = RefreshToken.for_user(self.user)
         self.access_token = str(self.refresh.access_token)
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.access_token}")
-        self.image = SimpleUploadedFile("test_image.jpg", content=b'content', content_type="image/jpeg")
+#image handling
+        with open('/home/savera/Downloads/PHOTO.jpeg', 'rb') as f:
+            image_data = f.read()
+        self.image = SimpleUploadedFile(name='image.jpg', content=image_data, content_type='image/jpeg')
 
     
 
@@ -50,13 +52,13 @@ class APIEndpointsTests(APITestCase):
 
     def test_createblogs(self):
         url = reverse('create')
-        data = {"title":"Mera Bdla","content":"Mera Bdla content","category":self.category.id}
+        data = {"title":"Mera Bdla","content":"Mera Bdla content","category":self.category.id,"content_image":self.image}
         response = self.client.post(url,data)
         self.assertEqual(status.HTTP_201_CREATED,response.status_code)
 
     def test_updateblogs(self):
         url = reverse('update',args=[self.blog.id])
-        data = {"title":"Updated title","content":"Updated content","category":self.category.id}
+        data = {"title":"Updated title","content":"Updated content","category":self.category.id,"content_image":self.image}
         response = self.client.put(url,data)
         self.assertEqual(status.HTTP_200_OK,response.status_code)
 
@@ -68,6 +70,7 @@ class APIEndpointsTests(APITestCase):
     def test_Userblog(self):
         url = reverse('Userblog')
         response = self.client.get(url)
+        #print(response.data)
         self.assertEqual(status.HTTP_200_OK,response.status_code)
         self.assertGreater(len(response.data),0)
 
@@ -81,18 +84,18 @@ class APIEndpointsTests(APITestCase):
 
 #Add user info testing
 
-    # def test_userInfo(self):
-    #     url = reverse("adduserinfo")
-    #     data = {
-    #         "bio": "mera naam sardar h",
-    #         "profile_picture": self.image
-    #     }
-    #     response = self.client.post(url, data, format="multipart")  # Ensure 'multipart' format for file upload
-    #     self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+    def test_userInfo(self):
+        url = reverse("adduserinfo")
+        data = {
+            "bio": "mera naam sardar h",
+            "profile_picture": self.image
+        }
+        response = self.client.post(url, data, format="multipart")  # Ensure 'multipart' format for file upload
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
 
-    #     # Validate the response contains expected data
-    #     self.assertIn("msg", response.data)
-    #     self.assertEqual(response.data["msg"], "User Info added successfully")
+        # Validate the response contains expected data
+        self.assertIn("msg", response.data)
+        self.assertEqual(response.data["msg"], "User Info added successfully")
 
     def test_getuser(self):
         AddUserInfo.objects.create(user=self.user, bio="This is a test bio")
